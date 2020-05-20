b1de4febc7aa4dc7d9e2c7f77a1ccbd372dd53ac
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-20 06:43:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ class Student
       end
   end
   
-  def self.first_X_students_in_grade_10(x) 
+  def self.first_X_students_in_grade_10(num) 
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
