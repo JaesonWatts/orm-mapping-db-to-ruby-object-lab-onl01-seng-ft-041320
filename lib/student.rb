aa6879ae4b9104564791bbf96e541bb7cee5c916
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-20 06:44:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,7 +98,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    first_X_students_in_grade_10(1)
+    first_student = self.first_X_students_in_grade_10(1)
   end
   
   def self.all_students_in_grade_X(x) 
