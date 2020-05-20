0d6597a7575d77a57dbe6cc21d2ff7776eae623d
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-20 06:28:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,7 @@ class Student
   end
   
   def self.students_below_12th_grade 
-    sql = "SELECT COUNT(grade > 10) FROM students"
+    sql = "SELECT * FROM students WHERE (grade < 12)"
     
     DB[:conn].execute(sql)
     
