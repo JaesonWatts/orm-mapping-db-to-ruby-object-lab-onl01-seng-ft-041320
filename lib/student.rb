4028b2ef11efa539b35b552fbd32f2edffe2286f
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-20 06:29:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -99,8 +99,10 @@ class Student
     
   end
   
-  def self.all_students_in_grade_X 
-    
+  def self.all_students_in_grade_X(x) 
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = x
+    SQL
   end
   
 end
